d448519b97f68e3063c65d456533563263c06afe
davidnkmylose <caopw84@gmail.com>
2014-07-21 10:50:26 +0800

    by davidnkcao

---
+++ swift/base_1.swift
@@ -0,0 +1,3 @@
+var aa = 1;
+var bb = 2;
+println("aaaaa : \(aa+bb)");
---
+++ swift/demo.swift
@@ -0,0 +1,5 @@
+let aa = 1;
+var bb = 3;
+var cc : String = "davidnkcao";
+println("aa is : \(aa), bb is \(bb)!");
+println("cc is : \(cc)");
